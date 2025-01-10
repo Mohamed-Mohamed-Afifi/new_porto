e28950bdec0cedb199c7d5a3a1bc151ee817d8ec
Mohamed-Mohamed-Afifi <mohamed.org050@gmail.com>
2025-01-10 23:08:31 +0200

    adding lazyloading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { Component } from "react";
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Experience from "./components/experience/Expreience";
-import Services from "./components/services/Services";
-import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
-class App extends Component {
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+// Lazy load the components
+const Header = lazy(() => import("./components/header/Header"));
+const Nav = lazy(() => import("./components/nav/Nav"));
+const About = lazy(() => import("./components/about/About"));
+const Experience = lazy(() => import("./components/experience/Expreience"));
+const Services = lazy(() => import("./components/services/Services"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+const Testimonials = lazy(() =>
+  import("./components/testimonials/Testimonials")
+);
+const Contact = lazy(() => import("./components/contact/Contact"));
+const Footer = lazy(() => import("./components/footer/Footer"));
+
+class App extends React.Component {
   render() {
     return (
-      <>
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="spinner"></div>
+          </div>
+        }
+      >
         <Header />
         <Nav />
         <About />
@@ -21,8 +32,9 @@ class App extends Component {
         <Testimonials />
         <Contact />
         <Footer />
-      </>
+      </Suspense>
     );
   }
 }
+
 export default App;
